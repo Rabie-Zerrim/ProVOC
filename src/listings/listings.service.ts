bdfad4a359470efabb78b9c15030a6aceeabebbc
rabie-z <rabiezerrim@gmail.com>
2026-06-11 19:18:02 +0100

    chore: remove debug photo_reference log

--- src/listings/listings.service.ts
+++ src/listings/listings.service.ts
@@ -53,7 +53,6 @@ export class ListingsService {
           url: '',
           photo_reference: place.photos?.[0]?.name ?? null,
         };
-        console.log('PHOTO_REF:', place.photos?.[0]?.name ?? 'NO_PHOTO');
       });
       return mapped;
     } catch (err: any) {
